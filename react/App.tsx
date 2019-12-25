4b2c44ad5a267dcfb09f023f69dcb6d6b0d49f3e
alcarasj <jcalcaras@gmail.com>
2019-12-25 03:02:21 +0000

    Set theming and init home page.

--- react/App.tsx
+++ react/App.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { HomePage } from "./pages/HomePage";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import red from "@material-ui/core/colors/red";
 import orange from "@material-ui/core/colors/orange";
-import blue from "@material-ui/core/colors/blue";
 
 const theme = createMuiTheme({
 	palette: {
 		type: "dark",
-		primary: orange,
-		secondary: blue
+		primary: red,
+		secondary: orange
 	}
 });
 
--- react/pages/HomePage.tsx
+++ react/pages/HomePage.tsx
@@ -1,13 +1,53 @@
 import React from "react";
 import { CustomAppBar } from "../components/CustomAppBar";
+import Avatar from '@material-ui/core/Avatar';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		root: {
+			flexGrow: 1,
+		},
+		me: {
+			height: theme.spacing(30),
+			width: theme.spacing(30)
+		},
+		body: {
+			marginTop: theme.spacing(5),
+			marginBottom: theme.spacing(20)
+		}
+	}),
+);
+
+const STATIC_DIR = "../../../static/"
 
 export interface HomePageProps { compiler: string; framework: string; }
 
 export const HomePage = (props: HomePageProps) => {
-	
+	const classes = useStyles({});
+
 	return (
-		<div>
+		<div className={classes.root}>
 			<CustomAppBar />
+			<Paper>
+				<Grid container justify="center" alignItems="center" direction="column">
+					<Grid item xs={12}>
+						<Grid className={classes.body} container justify="space-between" alignItems="center" spacing={5}  direction="column">
+							<Grid item xs={4}>
+								<Avatar className={classes.me} alt="Jerico Alcaras" src={STATIC_DIR + "img/jerico-2019-460x460.jpg"}/>
+							</Grid>
+							<Grid item xs={8}>
+								<Typography align="center" variant="h3" component="h3">Hi! My name is Jerico.</Typography>
+								<Typography align="center" variant="subtitle1">This is my personal website - it's currently under construction. Maybe check back again soon? :)</Typography>
+							</Grid>
+						</Grid>
+					</Grid>
+				</Grid>
+			</Paper>
 		</div>
 	);
 };
