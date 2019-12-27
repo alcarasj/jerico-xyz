6e8635635d6b14afb76687b07d520703c5e2993b
Jerico Alcaras <alcarasj@tcd.ie>
2019-12-27 22:21:43 +0000

    [0.1.5] Add Soundcloud playlist embed.

--- react/pages/HomePage.tsx
+++ react/pages/HomePage.tsx
@@ -71,6 +71,9 @@ export const HomePage = (props: HomePageProps) => {
 									<Typography align="center" variant="subtitle1" gutterBottom>SOFTWARE ENGINEER • GRAPHIC DESIGNER • MUSICIAN</Typography>
 								</Grow>
 							</Grid>
+							<Grid item xs>
+								<iframe width="100%" height="450" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/545610837&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+							</Grid>
 						</Grid>
 					</Grid>
 					<Grid item xs>
