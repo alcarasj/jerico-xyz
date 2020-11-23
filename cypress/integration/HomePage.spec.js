b19061bc67ce95044b7f09e7798b8d67b2af6625
Jerico Alcaras <jcalcaras@gmail.com>
2020-11-23 21:52:34 +0000

    Add Cypress report dirs to gitignore.

--- cypress/integration/HomePage.spec.js
+++ cypress/integration/HomePage.spec.js
@@ -2,5 +2,10 @@ describe('Home page spec', () => {
   it('Should show correct contents', () => {
     cy.visit('/');
     cy.get('#appbar').should('be.visible');
+    cy.get('h3').should('be.visible');
+    cy.get('h6').should('be.visible');
+    cy.get('.MuiCard-root').should(cards => {
+      expect(cards).to.have.length(3);
+    });
   });
 });
